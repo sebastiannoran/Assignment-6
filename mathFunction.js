02c36154cc1118a73cc0871d876ad14ebb1c8721
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-20 13:14:28 -0400

    Initial Commit

---
+++ mathFunction.js
@@ -0,0 +1,23 @@
+function add (a, b) {
+    return a + b;
+}
+
+module.exports = add;
+
+function subtract (a,b) {
+    return a - b;
+}
+
+module.exports = subtract;
+
+function multiply (a,b) {
+    return a * b;
+}
+
+module.exports = multiply;
+
+function divide (a,b) {
+    return a / b;
+}
+
+module.exports = divide;
