f80706cbbf96c765228355f11c79b506e8baca18
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-20 15:39:25 -0400

    reverse complete, palindrone incomplete

--- reverseString.js
+++ reverseString.js
@@ -1,6 +1,10 @@
 //use built in js function to spilt, reverse, and rejoin a string of words
 let reverseString = function (str) {
+    let spiltString = str.split("");
+    let reverseString = spiltString.reverse("");
+    let rejoinedString = reverseString.join("");
 
+    return rejoinedString;
 }
 
 let isPalindrone = function (str) {
--- reverseString.test.js
+++ reverseString.test.js
@@ -1,1 +1,17 @@
-const {reverseString, isPalindrone} = requre('./reverseString');
+const {reverseString, isPalindrone} = require('./reverseString');
+
+describe ("reverseString function should return backwards version of string", () => {
+    test ("reverseString(hello) => olleh", () => {
+        expect(reverseString("hello")).toEqual("olleh")
+    })
+    test ("reverseString(bob mob) => bom bob", () => {
+        expect(reverseString("bob mob")).toEqual("bom bob")
+    })
+    test ("reverseString(&&^^**##) => ##**^^&&", () => {
+        expect(reverseString("&&^^**##")).toEqual("##**^^&&")
+    })
+})
+
+describe ("isPalindrone function should return a boolean flag to indicate true if string is a palindrone, false if not"), () => {
+    
+}
